514fc2cd90c3aa3260471f4e7a0dc3e06bfc807a
dsschapira <dsschapira@gmail.com>
2020-03-19 07:45:45 -0400

    react test 2

---
+++ react-tac-toe/precache-manifest.01347fa7436f1632333f1d66bf75e6e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3b18bcae0fff3a6babe75c2490f695",
+    "url": "/react-tac-toe/index.html"
+  },
+  {
+    "revision": "ef659fcd53cb5d343ba6",
+    "url": "/react-tac-toe/static/css/main.47c75b13.chunk.css"
+  },
+  {
+    "revision": "3976f14b756d878847ba",
+    "url": "/react-tac-toe/static/js/2.921c24ec.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-tac-toe/static/js/2.921c24ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef659fcd53cb5d343ba6",
+    "url": "/react-tac-toe/static/js/main.bf90ef60.chunk.js"
+  },
+  {
+    "revision": "a7bd555a74e383f82a37",
+    "url": "/react-tac-toe/static/js/runtime-main.02c07609.js"
+  }
+]);

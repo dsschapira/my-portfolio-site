4f71f2f297e8d0059772061ba522ce8fb6299f0c
dsschapira <dsschapira@gmail.com>
2020-03-19 07:40:09 -0400

    test adding react node

---
+++ react-tac-toe/precache-manifest.55a161a7cfca1c5e55d361469825c5ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a518f68b08d9d0dcabb880092496b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "388b0db1fc05dbd97f3b",
+    "url": "/static/css/main.47c75b13.chunk.css"
+  },
+  {
+    "revision": "3976f14b756d878847ba",
+    "url": "/static/js/2.921c24ec.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.921c24ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388b0db1fc05dbd97f3b",
+    "url": "/static/js/main.d8f24853.chunk.js"
+  },
+  {
+    "revision": "b2920b87acc9a891b88b",
+    "url": "/static/js/runtime-main.d28f6590.js"
+  }
+]);
